385b44816e784b87e0bcbba9167526cf55a7c585
Luiz Leal <116567691+luiz-developer@users.noreply.github.com>
2023-03-20 18:25:05 -0300

    Update who-to-follow-content.js

--- js/who-to-follow-content.js
+++ js/who-to-follow-content.js
@@ -7,16 +7,19 @@ const fetchUsers = async () => {
 
    for (var i = 0; i < userJson.length; i++) {
       
+      var reformerName;
+      var reformerUsername;
+      
       if (userJson[i].name.length > 15) {
-         var reformerName = await userJson[i].name.slice(0, 12) + "...";
+         reformerName = await userJson[i].name.slice(0, 12) + "...";
       } else {
-         var reformerName = userJson[i].name;
+         reformerName = userJson[i].name;
       }
       
       if (userJson[i].username.length > 14) {
-         var reformerUsername = await userJson[i].username.slice(0, 14) + "...";
+         reformerUsername = await userJson[i].username.slice(0, 14) + "...";
       } else {
-         var reformerUsername = userJson[i].username;
+         reformerUsername = userJson[i].username;
       }
       
       suggestionContent.innerHTML += `
@@ -24,12 +27,14 @@ const fetchUsers = async () => {
          <div class="all-infors">
             <img src="https://avatars.githubusercontent.com/u/${userJson[i].githubId}?s=60&v=4" class="sugg-profile-picture" alt="">
                <div class="sugg-profile-infors">
-                  <div class="sugg-name">
-                     <span>${reformerName}</span>
-                  </div>
-                  <div class="sugg-username">
-                     <span>@${reformerUsername}</span>
-                  </div>
+               <a href="https://github.com/${userJson[i].username}">
+                     <div class="sugg-name">
+                        <span>${reformerName}</span>
+                     </div>
+                     <div class="sugg-username">
+                        <span>@${reformerUsername}</span>
+                     </div>
+                  </a>
                </div>
             </div>
          <button class="follow-btn ${userJson[i].following}">Follow</button>
