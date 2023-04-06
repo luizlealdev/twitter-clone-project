7ad313ca819fa201dba6c4cdde2acade167456cf
Luiz Leal <116567691+luiz-developer@users.noreply.github.com>
2023-04-06 15:19:26 -0300

    Firebase configuration file added

---
+++ js/firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+   apiKey: "AIzaSyAziM4-zzb85vE1x0uGsuevWGvmD5Gon8g",
+   authDomain: "twitter-clone-ldev.firebaseapp.com",
+   databaseURL: "https://twitter-clone-ldev-default-rtdb.firebaseio.com",
+   projectId: "twitter-clone-ldev",
+   storageBucket: "twitter-clone-ldev.appspot.com",
+   messagingSenderId: "650170960458",
+   appId: "1:650170960458:web:9cb92b92958a42341734b3",
+   measurementId: "G-6KKBT6F7EC",
+};
+
+firebase.initializeApp(firebaseConfig)
