52da97537fb64491c7e42f7426284f77a7352fb5
Luiz Leal <116567691+luiz-developer@users.noreply.github.com>
2023-04-06 15:21:03 -0300

    Script for tweet textarea

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+//var or elements
+const tweetTextArea = document.querySelector(".tweet-textarea");
+const caracterInfor = document.querySelector(".caracter-infor");
+
+//when somebody to click, activate the function 
+tweetTextArea.addEventListener("input", () => {
+   tweetTextArea.style.height = "auto";
+   caracterInfor.innerText = `${tweetTextArea.value.length}/280`;
+   tweetTextArea.style.height = `${tweetTextArea.scrollHeight}px`;
+});
+
 alert(
    "Hello, this site is under development. Don't be alarmed if something doesn't work."
 );
