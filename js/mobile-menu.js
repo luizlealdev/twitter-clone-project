31567ce070a372bedb268cc84de8dc8a0c6d5f5e
luiz-developer <luizandreleal100@gmail.com>
2023-04-25 18:13:03 -0300

    Mobile menu added

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,5 +1,27 @@
 const btnOpenMenu = document.querySelector('.menu-mobile-profile-pic');
+const overlay = document.querySelector('.overlay');
+mobileMenu = document.querySelector('.left-menu');
+toggleMenu = false;
 
-btnOpenMenu.addEventListener("click",{
-   alert("Olá ")
-});
+btnOpenMenu.addEventListener("click",clickMenu) 
+overlay.addEventListener("click",clickMenu) 
+   
+function clickMenu () {
+   if (toggleMenu == false) {
+      toggleMenu = true;
+      mobileMenu.classList.remove("closed");
+      overlay.style.display = "flex";
+      mobileMenu.classList.add("open");
+      overlay.classList.add("show")
+   } else {
+      toggleMenu = false;
+      mobileMenu.classList.remove("open");
+      mobileMenu.classList.add("closed");
+      overlay.classList.remove("show")
+      setTimeout(function() {
+         overlay.style.display = "none";
+      }, 300);
+   }
+   
+   return toggleMenu;
+}
